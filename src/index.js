97885aac033c65e7c64000c75fa1c3a32c9b3a8a
Alejandro <alejandroaperez1994g@gmail.com>
2023-03-28 15:41:08 +0200

    feat: :sparkles: added new library for logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const app = require('./server')
 const config = require('./config/config')
+const logger = require('log4js').getLogger()
+
+logger.level = 'debug'
 
 app.listen(config.app.PORT, () => {
-  config.logger.info(`⚙️ Server running at http:localhost:${config.app.PORT}`)
+  logger.info(`⚙️ Server running at http:localhost:${config.app.PORT}`)
 })
