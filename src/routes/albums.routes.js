d1343f0a07d3d4bbf0aaa5007e13b69d17d65e72
Alejandro <alejandroaperez1994g@gmail.com>
2023-03-28 15:39:57 +0200

    refactor: :art: updated the use of middlewares in album routes

--- src/routes/albums.routes.js
+++ src/routes/albums.routes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const albumsController = require('../controllers/albums.controller')
 const { checkParam, doubleCheckParam } = require('../middlewares/check-middleware')
+
 const router = express.Router()
 
 router
   .get('/', albumsController.allAlbums)
   .get('/:id', checkParam, doubleCheckParam, albumsController.singleAlbum)
-  .delete('/:id', checkParam, doubleCheckParam, albumsController.deleteAlbum)
-  .patch('/:id', checkParam, doubleCheckParam, albumsController.updateAlbum)
+  .delete('/:id', albumsController.deleteAlbum)
+  .patch('/:id', albumsController.updateAlbumArtist)
   .post('/', albumsController.createAlbum)
 
 module.exports = router
