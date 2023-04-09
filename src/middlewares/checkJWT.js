086f15e6ac90c188f3ff8da2a47f8eaaf55967de
Alejandro <alejandroaperez1994g@gmail.com>
2023-04-09 10:15:12 +0200

    feat: :recycle: refactor middleware

--- src/middlewares/checkJWT.js
+++ src/middlewares/checkJWT.js
@@ -1,36 +1,33 @@
 const config = require('../config/config')
 const jwt = require('jsonwebtoken')
+const UserModel = require('../models/user.model')
 
-const checkJWT = (req, res, next) => {
+const checkJWT = async (req, res, next) => {
   const token = req.header('x-token')
 
   if (!token) {
     return res.status(401).json({
-      msg: 'No hay token en la petición'
+      msg: 'There is no token in the request'
     })
   }
 
   try {
+    /* `const { id } = jwt.verify(token, config.jwt.private_key)` is verifying the JWT token received
+    in the request header by using the `jwt.verify()` method from the `jsonwebtoken` library. If the
+    token is valid, it will decode the payload and extract the `id` property from it. The
+    `config.jwt.private_key` is the private key used to sign the JWT token. The `id` extracted from
+    the token is then used to find the corresponding user in the database. */
     const { id } = jwt.verify(token, config.jwt.private_key)
     console.log(id)
 
-    // // leer el usuario que corresponde al uid
-    // const usuario = await Usuario.findById( uid );
+    const user = await UserModel.findById(id)
 
-    // if( !usuario ) {
-    //     return res.status(401).json({
-    //         msg: 'Token no válido - usuario no existe DB'
-    //     })
-    // }
+    if (!user) {
+      return res.status(401).json({
+        msg: 'Invalid token - user does not exist DB'
+      })
+    }
 
-    // // Verificar si el uid tiene estado true
-    // if ( !usuario.estado ) {
-    //     return res.status(401).json({
-    //         msg: 'Token no válido - usuario con estado: false'
-    //     })
-    // }
-
-    // req.usuario = usuario
     next()
   } catch (error) {
     console.log(error)
